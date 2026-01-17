49502fa868cef569a7791b001090d3f80ebfb971
shreeram312 <126177107+shreeram312@users.noreply.github.com>
2026-01-17 23:17:02 +0530

    refactor: enhance database connection logic with caching and error handling

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,13 +1,46 @@
-import { env } from "@veterinary-app/env/server";
 import mongoose from "mongoose";
 
-const dbName = "myDB";
+type dbCache = {
+  conn: typeof mongoose | null;
+  promise: Promise<typeof mongoose> | null;
+};
 
-await mongoose.connect(env.DATABASE_URL, { dbName }).catch((error) => {
-  console.log("Error connecting to database:", error);
-});
+const mongoGlobal = global as typeof globalThis & {
+  mongoose: dbCache;
+};
 
-const client = mongoose.connection.getClient().db(dbName);
+const DATABASE_URL = process.env.DATABASE_URL as string;
 
-export { client };
+if (!DATABASE_URL) {
+  throw new Error("Please define the DATABASE_URL environment variable");
+}
+
+const cached = mongoGlobal.mongoose ?? { conn: null, promise: null };
+mongoGlobal.mongoose = cached;
+async function dbConnect(): Promise<typeof mongoose> {
+  if (cached.conn) {
+    return cached.conn;
+  }
+  if (!cached.promise) {
+    const opts = {
+      bufferCommands: false,
+    };
+
+    cached.promise = mongoose.connect(DATABASE_URL, opts).then((mongoose) => {
+      return mongoose;
+    });
+  }
+
+  try {
+    cached.conn = await cached.promise;
+  } catch (e) {
+    cached.promise = null;
+    throw e;
+  }
+
+  return cached.conn;
+}
+const conn = await dbConnect();
+const db = conn.connection.db;
+export { db };
 export * from "./models/auth.model";
